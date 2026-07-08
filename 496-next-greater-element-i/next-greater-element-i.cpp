43d5ce1663d103113cbedc24ebd3726d3708962f
Ishann Parikh <ishaanparikh0706@gmail.com>
2026-07-08 12:41:36 +0530

    Time: 1 ms (53.67%) | Memory: 12.7 MB (62.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,29 +3,48 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans ;
         
-        for(int i = 0 ; i < nums1.size() ; i++){
-            bool present = false ;
-            for(int j = 0 ; j < nums2.size(); j++){
-                if(nums1[i]==nums2[j]){
-                    for(int k = j +1 ; k < nums2.size() ; k++){
-                        if(nums2[k]>nums2[j]){
-                            present = true ;
-                            ans.push_back(nums2[k]) ;
-                            break ;
+        // for(int i = 0 ; i < nums1.size() ; i++){
+        //     bool present = false ;
+        //     for(int j = 0 ; j < nums2.size(); j++){
+        //         if(nums1[i]==nums2[j]){
+        //             for(int k = j +1 ; k < nums2.size() ; k++){
+        //                 if(nums2[k]>nums2[j]){
+        //                     present = true ;
+        //                     ans.push_back(nums2[k]) ;
+        //                     break ;
 
-                        }
-                    }
-                    if(!present){
-                        ans.push_back(-1) ;
-                    }
+        //                 }
+        //             }
+        //             if(!present){
+        //                 ans.push_back(-1) ;
+        //             }
 
-                }
+        //         }
                 
                 
-            }
+        //     }
            
+        // }
+        // return ans ;
+
+        //better SOLUTION
+        unordered_map<int , int > mp ;
+        stack<int> s ;
+        for(int i = nums2.size() -1 ; i >= 0 ; i--){
+              while(!s.empty() && s.top() <= nums2[i]){
+            s.pop() ;
+        }
+        mp[nums2[i]] = s.empty() ? -1 : s.top() ;
+        s.push(nums2[i]);
+        }
+      
+
+        for(auto x : nums1){
+            ans.push_back(mp[x]) ;
         }
         return ans ;
+
+
         
     }
 };
